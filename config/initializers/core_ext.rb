ef3dfa9eba5413823e02780c0c69d133ea2973f4
Pierre de La Morinerie <kemenaran@gmail.com>
2021-10-12 17:34:41 +0200

    config: update core extensions on code reload
    This allows code in core extensions to be auto-reloaded as well as Rails
    classes.

--- config/initializers/core_ext.rb
+++ config/initializers/core_ext.rb
@@ -1,9 +1,11 @@
-# Load Ruby class extensions
-Dir.glob(Rails.root.join('lib/core_ext/**/*.rb')).each do |core_ext_file|
-  require core_ext_file
-end
+Rails.application.reloader.to_prepare do
+  # Load Ruby class extensions
+  Dir.glob(Rails.root.join('lib/core_ext/**/*.rb')).each do |core_ext_file|
+    load core_ext_file
+  end
 
-# Load Rails class extensions
-Dir.glob(Rails.root.join('app/lib/core_ext/**/*.rb')).each do |core_ext_file|
-  require core_ext_file
+  # Load Rails class extensions
+  Dir.glob(Rails.root.join('app/lib/core_ext/**/*.rb')).each do |core_ext_file|
+    load core_ext_file
+  end
 end
